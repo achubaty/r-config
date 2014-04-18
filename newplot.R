97d6e02ee795b20bb01bb5e04dce8fc123dcf206
Alex Chubaty <alex.chubaty@gmail.com>
2014-04-18 12:02:57 -0700

    added "newplot.R"

---
+++ newplot.R
@@ -0,0 +1,30 @@
+##############################################################
+#' Open a new plotting window
+#'
+#' Launch a new graphics device based on operating system used.
+#' Mac OS: open device with \code{quartz()}.
+#' Linux: open device with \code{x11()}.
+#' Windows: open device with \code{windows()}.
+#' 
+#' @return Opens a new plot device on the screen.
+#' 
+#' @param ... Additional arguments passed to x11() or windows() 
+#' 
+#' @export
+#' @docType methods
+#' @rdname newPlot
+#'
+# @examples
+# needs examples
+newPlot = function(...) {
+  if (Sys.info()[["sysname"]]=="Darwin") {
+    quartz(...)
+  } else if (Sys.info()[["sysname"]]=="Linux") {
+    x11(...)
+  } else if (Sys.info()[["sysname"]]=="Windows") {
+    windows(...)
+  } else {
+    x11(...) # try x11() to see if it works
+    print("Which operating system are you using?")
+  }
+}
