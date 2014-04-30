36f31b018372168cddea9c0df5c6d5e77329ae74
Alex Chubaty <alex.chubaty@gmail.com>
2014-04-30 12:04:47 -0700

    updated rdnames

--- newplot.R
+++ newplot.R
@@ -1,35 +1,4 @@
 ##############################################################
-#' Open a new plotting window
-#'
-#' Launch a new graphics device based on operating system used.
-#' Mac OS: open device with \code{quartz()}.
-#' Linux: open device with \code{x11()}.
-#' Windows: open device with \code{windows()}.
-#' 
-#' @param ... Additional arguments passed to quartz(), x11(), or windows().
-#' 
-#' @return Opens a new plot device on the screen.
-#' 
-#' @export
-#' @docType methods
-#' @rdname plot2
-#'
-# @examples
-# needs examples
-newPlot = function(...) {
-  if (Sys.info()[["sysname"]]=="Darwin") {
-    quartz(...)
-  } else if (Sys.info()[["sysname"]]=="Linux") {
-    x11(...)
-  } else if (Sys.info()[["sysname"]]=="Windows") {
-    windows(...)
-  } else {
-    x11(...) # try x11() to see if it works
-    print("Which operating system are you using?")
-  }
-}
-
-##############################################################
 #' Specify where to plot
 #'
 #' Switch to an existing plot device, or if not already open,
@@ -45,11 +14,42 @@ newPlot = function(...) {
 #' 
 #' @export
 #' @docType methods
-#' @rdname plot2-method
+#' @rdname plot2dev-method
 #'
 # @examples
 # needs examples
 plot2dev = function(x, ...) {
   if(is.null(dev.list())) newPlot(...)
   while (dev.set(x)!=x) newPlot(...)
+}
+
+##############################################################
+#' Open a new plotting window
+#'
+#' Launch a new graphics device based on operating system used.
+#' Mac OS: open device with \code{quartz()}.
+#' Linux: open device with \code{x11()}.
+#' Windows: open device with \code{windows()}.
+#' 
+#' @param ... Additional arguments passed to quartz(), x11(), or windows().
+#' 
+#' @return Opens a new plot device on the screen.
+#' 
+#' @export
+#' @docType methods
+#' @rdname plot2dev-method
+#'
+# @examples
+# needs examples
+newPlot = function(...) {
+  if (Sys.info()[["sysname"]]=="Darwin") {
+    quartz(...)
+  } else if (Sys.info()[["sysname"]]=="Linux") {
+    x11(...)
+  } else if (Sys.info()[["sysname"]]=="Windows") {
+    windows(...)
+  } else {
+    x11(...) # try x11() to see if it works
+    print("Which operating system are you using?")
+  }
 }
