6c1b2f228c46a27ea69f95e73e434026829dd1b2
Alex Chubaty <alex.chubaty@gmail.com>
2014-05-02 14:04:28 -0700

    added guesstimate

--- sysmem.R
+++ sysmem.R
@@ -7,7 +7,7 @@
 #' 
 #' NOTE: this *should* work on OSX but hasn't been tested yet.
 #' 
-#' @param  Units to use for output. One of either "kb", "mb", "gb".
+#' @param  x  Units to use for output. One of either "kb", "mb", "gb".
 #' 
 #' @return Total amount of system memory (RAM) in \code{GB}.
 #' 
@@ -52,5 +52,42 @@ sysmem = function(x="gb") {
     return(ram.gb)
   } else {
     stop() # error checking happens at start of function
+  }
+}
+
+##############################################################
+#' Guesstimate the number of cpus for cluster opreations
+#'
+#' Take a wild stab at guessing how many cpus to use in cluster when you
+#' have some idea of how much ram is needed per cpu.
+#' 
+#' Tries to be conservative by assuming no more than 80% system memory use.
+#' You should take these numbers with a grain of salt.
+#' 
+#' @param  ram  How much ram is required per cpu.
+#' 
+#' @param  units  Units of memory. One of either "kb", "mb", "gb".
+#' 
+#' @return number of cpus to allocate to cluster.
+#' 
+#' @export
+#' @docType methods
+#' @rdname sysmem-method
+#'
+#' @examples
+#' guesstimate(4, "gb")
+#' 
+guesstimate = function(ram, units="gb") {
+  if (ram>=sysmem(units)) {
+    stop("Not enough system memory.")
+  } else {
+    # 80% of sysmem may be too conservative / not conservative enough
+    # - adjust as needed.
+    if (ram>=0.8*sysmem(units)) {
+      warning("High RAM requirement for this system!", immediate.=TRUE)
+    }
+    cpus = floor( 0.8*sysmem(units) / ram )
+    if (cpus<1) cpus=1
+    return(cpus)
   }
 }
