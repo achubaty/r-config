9243a36051aa3a13dc2cdc1a3bb90f03400b866b
Alex Chubaty <alex.chubaty@gmail.com>
2014-05-14 11:18:51 -0700

    added `quiet`  arg to load.packages

--- load-packages.R
+++ load-packages.R
@@ -9,6 +9,9 @@
 #' @param install         Logical flag. If required packages are not
 #'                        already installed, should they be installed?
 #'
+#' @param quiet           Logical flag. Should the final "packages loaded"
+#'                        message be suppressed?
+#'
 #' @return Nothing is returned. Specified packages are loaded and attached using \code{library()}.
 #' 
 #' @seealso \code{\link{library}}.
@@ -27,7 +30,7 @@ setGeneric("load.packages", function(package.list, install) {
 
 #' @rdname loadpackages-method
 setMethod("load.packages",
-          signature(package.list="list", install="logical"),
+          signature(package.list="list", install="logical", quiet="logical"),
           definition = function(package.list, install) {
             load <- function(name, install) {
               if (!require(name, character.only=TRUE)) {
@@ -40,12 +43,26 @@ setMethod("load.packages",
               }
             }
             lapply(package.list, load, install)
-            print(paste("Loaded", length(package.list), "packages.", sep=" "))
+            if (!quiet) print(paste("Loaded", length(package.list), "packages.", sep=" "))
 })
 
 #' @rdname loadpackages-method
 setMethod("load.packages",
-          signature(package.list="list", install="missing"),
+          signature(package.list="list", install="missing", quiet="missing"),
           definition = function(package.list) {
-            load.packages(package.list=package.list, install=FALSE)
+            load.packages(package.list=package.list, install=FALSE, quiet=FALSE)
+})
+
+#' @rdname loadpackages-method
+setMethod("load.packages",
+          signature(package.list="list", install="missing", quiet="logical"),
+          definition = function(package.list) {
+            load.packages(package.list=package.list, install=FALSE, quiet=quiet)
+})
+
+#' @rdname loadpackages-method
+setMethod("load.packages",
+          signature(package.list="list", install="logical", quiet="missing"),
+          definition = function(package.list) {
+            load.packages(package.list=package.list, install=install, quiet=FALSE)
 })
