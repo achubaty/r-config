2e67c4e8484008b21f37eec9b9ec7320f0c8db3d
Alex Chubaty <alex.chubaty@gmail.com>
2014-07-18 23:13:08 -0700

    reordered inRange() error checks for logic

--- inRange.R
+++ inRange.R
@@ -1,9 +1,9 @@
 # Test whether a number lies within range [a,b]
 inRange <- function(x, a=0, b=1) {
-  if (is.na(a) || is.na(b)) stop("invalid (NA) bounds.")
-  if (!is.numeric(a) || !is.numeric(b)) stop("invalid (non-numeric) bounds.")
-  if (a>=b) stop("a cannot be greater than b.")
-  if (!is.numeric(x)) stop("x must be numeric.")
   if (is.null(x)) return(NULL) # is this desired behaviour?
+  if (!is.numeric(x)) stop("x must be numeric.")
+  if (!is.numeric(a) || !is.numeric(b)) stop("invalid (non-numeric) bounds.")
+  if (is.na(a) || is.na(b)) stop("invalid (NA) bounds.")
+  if (a>=b) stop("a cannot be greater than b.")
   return((x - a)  *  (b - x) >= 0) # NAs will propagate -- is this desired?
 }
