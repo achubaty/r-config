adea28db400780b0512bf6104a827d9cdfd4383c
Alex Chubaty <alex.chubaty@gmail.com>
2016-10-28 13:52:25 -0600

    fix bug in dl.data

--- download-data.R
+++ download-data.R
@@ -1,16 +1,16 @@
 #' Intelligently download data
-#' 
+#'
 #' Only downloads the specified files if it is not found locally.
 #' Optionally unzips the files.
-#' 
+#'
 #' @param urls      A character vector of data file URLs.
 #' @param dest      The directory path in which data should be downloaded.
 #' @param checksum  Logical indicating whether downloaded files should be checksummed to verify them.
 #' @param unzip     Logical indicating whether the file should be unzipped after download.
-#' 
+#'
 #' @author Alex Chubaty and Eliot Mcintire
 #' @importFrom digest digest
-#' 
+#'
 dl.data <- function(urls, dest = ".", checksum = TRUE, unzip = FALSE) {
   tmp <- lapply(urls, function(f) {
     dest_file <- file.path(dest, basename(f))
@@ -39,13 +39,13 @@ dl.data <- function(urls, dest = ".", checksum = TRUE, unzip = FALSE) {
         needDownload <- FALSE
       }
     }
-    
+
     if (needDownload)  {
-      download.file(url, dest_file)
+      download.file(f, dest_file)
       checksum <- digest::digest(file = dest_file, algo = "xxhash64")
       write.table(data.frame(filename = dest_file, checksum = checksum),
                   file = checksum_file)
-      
+
       if (unzip) { unzip(dest_file, exdir = dest, overwrite = TRUE) }
     }
   })
