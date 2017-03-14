16e898cfc639c984055951198714b0960f0b1145
Alex Chubaty <alex.chubaty@gmail.com>
2017-03-14 15:21:34 -0600

    Update means.R

--- means.R
+++ means.R
@@ -1,10 +1,10 @@
 geometric.mean = function(x, ...) {
-    gm = prod(x, ...)^(1/length(x))
+    gm = prod(x, ...) ^ (1 / length(x))
     return(gm)
 }
 
 harmonic.mean = function(x, ...) {
-    if (length(x[x==0])) stop("Error: contains zero values.")
-    hm = 1/mean(1/x, ...)
+    if (length(x[x == 0])) stop("Error: contains zero values.")
+    hm = 1 / mean(1 / x, ...)
     return(hm)
 }
