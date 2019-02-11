6a87b20008bbd80bf788a7b1ec01b04d26148a1d
Alex Chubaty <alex.chubaty@gmail.com>
2019-02-10 17:17:36 -0700

    R-devel update: don't update packages post-update

--- R-devel/scripts/update-R-devel.sh
+++ R-devel/scripts/update-R-devel.sh
@@ -13,9 +13,3 @@ bash ./build-R-devel.sh
 # make and install
 cd ~/GitHub/r-config/R-devel/src/R-devel-build
 sudo make install
-
-# update R packages
-echo "*** Installation complete. Updating R packages..."
-Rscript-devel -e 'install.packages("lattice", .Library.site[1])'
-Rscript-devel -e 'update.packages(ask = FALSE, checkBuilt = TRUE, lib.loc = .Library.site[1])'
-echo "*** Done."
