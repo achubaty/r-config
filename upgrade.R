ceeb5bd54bd35176356339e4960cd4bc0196969c
Alex Chubaty <alex.chubaty@gmail.com>
2020-05-22 11:46:12 -0600

    misc updates

---
+++ upgrade.R
@@ -0,0 +1,20 @@
+## update R packages following major version update
+libDir <- "~/R/x86_64-pc-linux-gnu-library"
+oldVer <- "3.6"
+newVer <- "4.0"
+
+oldLibDir <- file.path(libDir, oldVer)
+newLibDir <- file.path(libDir, newVer)
+
+pkgs <- as.data.frame(installed.packages(oldLibDir), stringsAsFactors = FALSE)
+ids <- which(!(pkgs$Priority %in% c("base", "recommended")))
+pkgs <- pkgs[ids, ]
+pkgs <- pkgs$Package
+
+## install CRAN packages
+install.packages(pkgs[amc::pkgSrc(pkgs, oldLibDir) == "CRAN"], newLibDir)
+
+## install GitHub packages
+ids <- which(!(amc::pkgSrc(pkgs, oldLibDir) %in% c("CRAN", "Bioconductor", "Other")))
+sapply(amc::pkgSrc(pkgs, oldLibDir)[ids], devtools::install_github)
+
